abb80fdb68700203dc2ad1ed5ce5b66e8bf76b44
dkamenkov <dkamenkov@croc.ru>
2023-10-16 19:21:33 +0700

    any fix for main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import boto3
 import botocore
 from dotenv import load_dotenv
+from pathlib import Path
 import os
 import subprocess
 import logging
@@ -8,41 +9,52 @@ import requests
 from datetime import datetime
 import concurrent.futures
 import html
+import time
 
-
-load_dotenv()
+# Initialize logging and load environment variables
 logging.basicConfig(filename='logs.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+env_path = Path(__file__).parent / ".env"
+load_dotenv(env_path)
 
 class ApiChecker:
-    def __init__(self):
-        self.alert_sent = False
-        self.mtr_url = os.getenv('ENDPOINT_URL')
-        self.endpoint_url = "https://" + self.mtr_url
-        self.TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-        self.TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+    def __init__(self) -> None:
+        self.alert_sent: bool = False
+        self.mtr_url: str = os.getenv('ENDPOINT_URL')
+        self.endpoint_url: str = f"https://{self.mtr_url}"
+        self.TELEGRAM_BOT_TOKEN: str = os.getenv('TELEGRAM_BOT_TOKEN')
+        self.TELEGRAM_CHAT_ID: str = os.getenv('TELEGRAM_CHAT_ID')
 
-    def get_external_ip(self):
+    def get_external_ip(self) -> str:
+        """Retrieve the external IP address of the current machine."""
+        logging.info("Retrieving external IP...")
         try:
-            response = requests.get("https://httpbin.org/ip", timeout=5)  # добавлен таймаут
+            response = requests.get("https://httpbin.org/ip", timeout=5)
             ip = response.json()["origin"]
+            logging.info(f"Successfully retrieved external IP: {ip}")
             return ip
         except Exception as e:
-            logging.error(f"Error getting external IP: {str(e)}")
+            logging.error(f"Error getting external IP: {e}")
             return "Unknown IP"
-        
-    def is_on_maintenance(self, endpoint):
+
+
+    def is_on_maintenance(self, endpoint: str) -> bool:
+        """Determine if the API is on maintenance mode by checking for 'OnMaintenance' in the response."""
+        logging.info("Checking if API is on maintenance...")
         try:
             response = requests.get(endpoint)
             if "OnMaintenance" in response.text:
+                logging.info("API is on maintenance.")
                 return True
+            logging.info("API is not on maintenance.")
             return False
         except Exception as e:
-            logging.error(f"Error checking maintenance status: {str(e)}")
+            logging.error(f"Error checking maintenance status: {e}")
             return False
         
-    def telegram_sender(self, text):
+    def telegram_sender(self, text: str) -> None:
+        """Send a specified message to a Telegram chat."""
+        logging.info("Sending message to Telegram...")
         url = f"https://api.telegram.org/bot{self.TELEGRAM_BOT_TOKEN}/sendMessage"
-
         payload = {
             'chat_id': self.TELEGRAM_CHAT_ID,
             'text': text,
@@ -50,83 +62,74 @@ class ApiChecker:
         }
 
         try:
-            response = requests.post(url, data=payload, timeout=5)  # добавлен таймаут
+            response = requests.post(url, data=payload, timeout=5)
             if response.status_code != 200:
                 logging.error(f"Failed to send message to Telegram: {response.content}")
         except Exception as e:
-            logging.error(f"An error occurred when trying to send message to Telegram: {str(e)}")
-            # Дополнительные действия при ошибке отправки уведомления...
+            logging.error(f"Error when trying to send message to Telegram: {e}")
 
-    def send_alert(self, mtr_url, mtr_output, error_message=None):
-        logging.info("Sending alert...")  # добавлено логгирование
+    def send_alert(self, mtr_url: str, mtr_output: str, error_message: str = None) -> None:
+        """Send an alert to Telegram regarding an issue with the API."""
+        logging.info("Compiling alert message...")
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        source_ip = self.get_external_ip()
 
-        # Получение IP-адреса текущего хоста
+        # Escape received values for safety
+        mtr_output = f"<pre>{html.escape(mtr_output)}</pre>"
+        error_message = f"<code>{html.escape(error_message)}</code>"
 
-        source_ip = self.get_external_ip()  # убрано дублирование
-
-        # Экранируем полученные значения перед их использованием
-        mtr_output = "<pre>" + html.escape(mtr_output) + "</pre>"
-        error_message = "<code>" + html.escape(error_message) + "</code>"
-
-        # Формирование сообщения
         alert_message = f"""
-<b>🚨 Обнаружена проблема с работоспособностью API {mtr_url} 🚨</b>
-<b>Дата/время фиксации проблемы:</b> {now}
-<b>Адрес источника обращения к API:</b> {source_ip}
-<b>Ссылка на регламент:</b> <a href='[Ваша ссылка здесь]'>Перейти к регламенту</a>
-<b>Ошибка:</b> {error_message}
-<b>Трассировка до {mtr_url}:</b>
-<pre>
+<b>🚨 Issue detected with API {mtr_url} 🚨</b>
+<b>Timestamp:</b> {now}
+<b>Source IP:</b> {source_ip}
+<b>Documentation:</b> <a href='[Your Link Here]'>Go to documentation</a>
+<b>Error:</b> {error_message}
+<b>Trace to {mtr_url}:</b>
 {mtr_output}
-</pre>
 """
+
         self.telegram_sender(alert_message)
 
-    def check_api(self):
+    def check_api(self) -> (bool, str):
+        """Attempt to access the API and return its status and any associated error message."""
+        logging.info("Checking API status...")
         session = boto3.Session(
             aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
             aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
         )
-        # Установка таймаутов для сессии
-        config = botocore.config.Config(
-            connect_timeout=5,  # Установка таймаута подключения
-            read_timeout=10,    # Установка таймаута чтения
-        )
 
-        client = session.client(
-            'ec2',
-            endpoint_url=self.endpoint_url,
-            config=config,
-        )
-        error = None
+        # Set timeouts for the session
+        config = botocore.config.Config(connect_timeout=5, read_timeout=10)
+
+        client = session.client('ec2', endpoint_url=self.endpoint_url, config=config)
+
         try:
-            logging.info("Calling describe_availability_zones()...")  # Добавлено логгирование
+            logging.info("Calling describe_availability_zones()...")
             response = client.describe_availability_zones()
             logging.info("describe_availability_zones() call completed.")
-            return True, None
+            return True, ""
         except botocore.exceptions.EndpointConnectionError as e:
-            logging.error(f"Cannot connect to the endpoint: {str(e)}")
-            error = f"Cannot connect to the endpoint: {str(e)}"
+            logging.error(f"Cannot connect to the endpoint: {e}")
+            return False, f"Cannot connect to the endpoint: {e}"
         except botocore.exceptions.PartialCredentialsError as e:
-            logging.error(f"Incomplete credentials provided: {str(e)}")
-            error = f"Incomplete credentials provided: {str(e)}"
+            logging.error(f"Incomplete credentials provided: {e}")
+            return False, f"Incomplete credentials provided: {e}"
         except botocore.exceptions.SSLError as e:
-            logging.error(f"SSL/TLS error occurred: {str(e)}")
-            error = f"SSL/TLS error occurred: {str(e)}"
+            logging.error(f"SSL/TLS error occurred: {e}")
+            return False, f"SSL/TLS error occurred: {e}"
         except botocore.exceptions.ClientError as e:
             error_code = e.response['Error']['Code']
             error_message = e.response['Error']['Message']
             logging.error(f"ClientError occurred: {error_code} - {error_message}")
-            error = f"ClientError occurred: {error_code} - {error_message}"
+            return False, f"ClientError occurred: {error_code} - {error_message}"
         except Exception as e:
-            logging.error(f"An unexpected error occurred: {str(e)}")
-            error = f"An unexpected error occurred: {str(e)}"
+            logging.error(f"Unexpected error: {e}")
+            return False, f"Unexpected error: {e}"
 
-        return False, error
 
-    def run_mtr(self, target):
-        target = self.mtr_url
+    def run_mtr(self, target: str) -> str:
+        """Execute MTR to get a network trace to the specified target."""
+        logging.info(f"Running MTR for target: {target}")
         try:
             result = subprocess.run(["mtr", "--report", "--report-cycles", "1", "-4", "--no-dns", target],
                                     stdout=subprocess.PIPE,
@@ -135,41 +138,48 @@ class ApiChecker:
                                     check=True)
             return result.stdout
         except subprocess.CalledProcessError as e:
-            logging.error(f"An error occurred: {str(e)}")
+            logging.error(f"MTR error: {e}")
             logging.error(f"stderr: {e.stderr}")
-            return None
+            return ""
 
-    def run(self):
-        logging.info("Starting main...")
-
-        if self.is_on_maintenance(self.endpoint_url):
-            logging.info("The service is on maintenance. Skipping checks.")
-            return
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            future = executor.submit(self.check_api)
-            try:
-                success, error_message = future.result(timeout=15)
-
-                if not success:
-                    if not self.alert_sent:
-                        mtr_output = self.run_mtr(self.mtr_url)
-                        if mtr_output:
-                            self.send_alert(self.mtr_url, mtr_output, error_message)
-                            self.alert_sent = True
-                        else:
-                            logging.error("Failed to get MTR trace.")
-                else:
-                    if self.alert_sent:
-                        self.telegram_sender(f"🟢 Проблема с API {self.mtr_url} решена!")
-                        self.alert_sent = False
-
-            except concurrent.futures.TimeoutError:
-                logging.error("API check timed out")
-                mtr_output = self.run_mtr(self.mtr_url)
-                if mtr_output:
-                    self.send_alert(self.mtr_url, mtr_output, "API check timed out")
-                else:
-                    logging.error("Failed to get MTR trace.")
+    def run(self) -> None:
+        """Main function to check the API and send alerts."""
+        logging.info("Initializing main run sequence...")
+    
+        while True:  # бесконечный цикл
+            if self.is_on_maintenance(self.endpoint_url):
+                logging.info("The service is on maintenance. Skipping checks.")
+                time.sleep(60)  # пауза в 60 секунд
+                continue
+            
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                future = executor.submit(self.check_api)
+    
+                try:
+                    success, error_message = future.result(timeout=15)
+    
+                    if not success:
+                        if not self.alert_sent:
+                            mtr_output = self.run_mtr(self.mtr_url)
+                            if mtr_output:
+                                self.send_alert(self.mtr_url, mtr_output, error_message)
+                                self.alert_sent = True
+                            else:
+                                logging.error("Failed to get MTR trace.")
+                    else:
+                        if self.alert_sent:
+                            self.telegram_sender(f"🟢 Issue with API {self.mtr_url} resolved!")
+                            self.alert_sent = False
+    
+                except concurrent.futures.TimeoutError:
+                    logging.error("API check timed out")
+                    mtr_output = self.run_mtr(self.mtr_url)
+                    if mtr_output:
+                        self.send_alert(self.mtr_url, mtr_output, "API check timed out")
+                    else:
+                        logging.error("Failed to get MTR trace.")
+    
+            time.sleep(60)  # пауза в 60 секунд перед следующей итерацией цикла
 
 
 if __name__ == "__main__":
